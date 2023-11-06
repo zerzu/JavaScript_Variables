bd448bf5a319be4b93703b626e99659c7bddce66
Eric Garner <ericgarner81@gmail.com>
2023-11-06 11:18:46 -0600

    completed exercise

--- app.js
+++ app.js
@@ -3,8 +3,80 @@ console.log(
   "Follow the steps in the README.md file to complete the exercises:\n==========\n"
 );
 
-// Exercise 1
+// ## Exercise 1: Declaring Variables
+
+// Steps are to be completed in the `app.js` file.
+
+// Declare a variable named lastName using the let keyword
+// Declare a variable named age using the var keyword
+
+let lastName = "";
+var age;
 
 // Exercise 2
 
+// Declare a const named firstName and assign it to a string that's your first name.
+// Assign your last name as the string value to the variable lastName
+// Assign your age as the number value to the variable age
+
+const firstName =
+{
+  string : "Eric"
+};
+
+lastName = "Garner";
+
+let age = 42;
+
 // Exercise 3
+
+// Declare a variable named language using the let keyword, and assign it the value "JavaScript" (string)
+// Declare a variable named createdYear using the let keyword, and assign it the value 1995 (number)
+// Declare a variable named isCaseSensitive using the let keyword, and assign it the value true (boolean)
+
+let language = "JavaScript";
+
+let createdYear = 1995;
+
+let isCaseSensitive = true;
+
+//exercise 4
+
+// Declare a variable named price using the let keyword, and assign it the value 19.99 (number)
+// Declare a variable named isOnSale using the let keyword, and assign it the value false (boolean)
+// Declare a variable named salePercentage using the let keyword, and assign it the value 15 (number)
+// Declare a variable named stock using the let keyword, and assign it the value 0 (number)
+// Declare a variable named inStock using the let keyword, and assign it the value false (boolean)
+// Declare a variable named selectedSize using the let keyword, and assign it the value "M" (string)
+
+let price = 19.99;
+
+let isOnSale = false;
+
+let salePercentage = 15;
+
+let stock = 0;
+
+let inStock = false;
+
+let selectedSize = "M";
+
+//exercise 5
+
+// Declare a variable named title using the let keyword, and assign it the value "Name of the Wind" (string)
+// Declare a variable named author using the let keyword, and assign it the value "Patrick Rothfuss" (string)
+// Declare a variable named pageCount using the let keyword, and assign it the value 722 (number)
+// Declare a variable named bookmark using the let keyword, and assign it the value 456 (number)
+// Declare a variable named hasRead using the let keyword, and assign it the value true (boolean)
+
+let title = "Name of the Wind";
+
+let author = "Patrick Rothfuss";
+
+let pageCount = 722;
+
+let bookmark = 456;
+
+let hasRead = true;
+
+var age;
